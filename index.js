2a831096d95802bad681973bc3235b70a7977f29
Sabina Hasanova <sabina.h@bestcomp.net>
2026-01-15 16:23:10 +0400

    bot changes

--- index.js
+++ index.js
@@ -2,36 +2,44 @@ import fs from 'fs';
 import cron from 'node-cron';
 import { checkZaraAvailability } from './scraper.js';
 import { sendTelegramNotification,sendTelegramErrorNotification  } from './notifier.js';
+import pLimit from 'p-limit';
+
+const limit = pLimit(3);
 
 
-
-
-async function runStockCheck() {
+ async function runStockCheck() {
   const products = JSON.parse(fs.readFileSync('./products.json'));
-  for (const product of products) {
-    if (product.status !== 1) continue;
 
-    try {
-      const inStock = await checkZaraAvailability(product);
+  const tasks = products
+    .filter(product => product.status === 1)
+    .map(product =>
+      limit(async () => {
+        try {
+          const inStock = await checkZaraAvailability(product);
 
-      if (inStock) {
-        await sendTelegramNotification(
-          product.userId,
-          `✅ In stock!\nSize: ${product.size || 'Any'}\n${product.url}`
-        );
-      }
+          if (inStock) {
+            await sendTelegramNotification(
+              product.userId,
+              `✅ In stock!\nSize: ${product.size || 'Any'}\n${product.url}`
+            );
+          }
 
-    } catch (err) {
-      console.log('Global stock check error:', err.message);
+        } catch (err) {
+          console.log('Global stock check error:', err.message);
 
-      await sendTelegramErrorNotification(
-        `❌ Stock checker crashed\nError: ${err.message}`
-      );
-    }
- 
-  }
+          await sendTelegramErrorNotification(
+            `❌ Stock checker crashed\nError: ${err.message}`
+          );
+        }
+      })
+    );
+
+  await Promise.all(tasks);
+
   fs.writeFileSync('./products.json', JSON.stringify(products, null, 2));
 }
+
+
 
 await runStockCheck();
 
