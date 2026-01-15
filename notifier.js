bd93f4b49a32b5940ef1a795a7890c47d41c7f0d
Sabina Hasanova <sabina.h@bestcomp.net>
2026-01-15 16:20:33 +0400

    //

--- notifier.js
+++ notifier.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const TELEGRAM_TOKEN = '7552377314:AAHpfHIsoTM4p3Iv5dhAEBt0E5WmWtnWeAk';
-const CHAT_ID='1279780050';
+
+const AUTHOR_CHAT_ID='1279780050';
+const TOKEN = process.env.BOT_TOKEN;
 
 export async function sendTelegramNotification(chatId, message) {
   await axios.post(
-    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
     { 
       chat_id: chatId,
       text: message
@@ -16,9 +17,9 @@ export async function sendTelegramNotification(chatId, message) {
 
 export async function sendTelegramErrorNotification(message) {
   await axios.post(
-    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+    `https://api.telegram.org/bot${TOKEN}/sendMessage`,
     { 
-      chat_id: CHAT_ID,
+      chat_id: AUTHOR_CHAT_ID,
       text: message
     }
   );
